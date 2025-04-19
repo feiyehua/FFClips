ba54538860da592adcda3202c857f5fa1fd003e3
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2025-04-19 19:27:53 +0800

    Remove unused code

--- FFClips/ContentView.swift
+++ FFClips/ContentView.swift
@@ -1,16 +1,15 @@
 //
-    /*******************************************************************************
-            
-            File name:     ContentView.swift
-            Author:        FeiYehua
-            
-            Description:   Created for FFClips in 2025
-            
-            History:
-                    2025/4/18: File created.
-            
-    ********************************************************************************/
-    
+/*******************************************************************************
+
+        File name:     ContentView.swift
+        Author:        FeiYehua
+
+        Description:   Created for FFClips in 2025
+
+        History:
+                2025/4/18: File created.
+
+********************************************************************************/
 
 //import SwiftUI
 //import PhotosUI
@@ -33,39 +32,11 @@ import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedItems = [PhotosPickerItem]()
-    @State private var selectedImages = [Image]()
-
     var body: some View {
-        NavigationStack {
-            MediaPickerView()
-            ScrollView {
-                LazyVStack {
-                    ForEach(0..<selectedImages.count, id: \.self) { i in
-                        selectedImages[i]
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 300, height: 300)
-                    }
-                }
-            }
-            .toolbar {
-                PhotosPicker("Select images", selection: $selectedItems, matching: .images)
-            }
-            .onChange(of: selectedItems) {
-                Task {
-                    selectedImages.removeAll()
-
-                    for item in selectedItems {
-                        if let image = try? await item.loadTransferable(type: Image.self) {
-                            selectedImages.append(image)
-                        }
-                    }
-                }
-            }
-        }
+        MediaPickerView()
     }
 }
+
 #Preview {
     ContentView()
 }
